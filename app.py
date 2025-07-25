092a129b79a2fe0bf91299b9ea6111b2c4d0726d
PxS00 <lucas.rossonidd@gmail.com>
2025-07-25 13:46:28 -0300

    Add initial application logic for restaurant management

---
+++ app.py
@@ -0,0 +1,18 @@
+print('𝓢𝓪𝓫𝓸𝓻 𝓔𝓼𝓹𝓻𝓮𝓼𝓼\n')
+
+print('1. Cadastrar restaurante')
+print('2. Listar restaurantes')
+print('3. Ativar restaurante')
+print('4. Sair\n')
+
+option_chosen = int(input('Escolha uma opção: '))
+
+if option_chosen == 1: 
+    print('Cadastrar restaurante')
+elif option_chosen == 2: 
+    print('Listar restaurantes')
+elif option_chosen == 2: 
+    print('Ativar restaurante')
+else:
+    print('Encerrando o programa...')
+
