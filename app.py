2910b51607e3d9b8f250cb19958d16198d76eba4
PxS00 <lucas.rossonidd@gmail.com>
2025-07-25 14:29:43 -0300

    Refactor app.py to modularize functionality with dedicated functions for displaying options, handling user input, and managing application flow.

--- app.py
+++ app.py
@@ -1,18 +1,42 @@
-print('𝓢𝓪𝓫𝓸𝓻 𝓔𝓼𝓹𝓻𝓮𝓼𝓼\n')
+import os
 
-print('1. Cadastrar restaurante')
-print('2. Listar restaurantes')
-print('3. Ativar restaurante')
-print('4. Sair\n')
+def display_name():
+    print('𝓢𝓪𝓫𝓸𝓻 𝓔𝓼𝓹𝓻𝓮𝓼𝓼\n')
 
-option_chosen = int(input('Escolha uma opção: '))
+def display_options():
+    print('1. Cadastrar restaurante')
+    print('2. Listar restaurantes')
+    print('3. Ativar restaurante')
+    print('4. Sair\n')
+    
+def end_app():
+    os.system('cls')
+    print('Encerrando app...\n')
 
-if option_chosen == 1: 
-    print('Cadastrar restaurante')
-elif option_chosen == 2: 
-    print('Listar restaurantes')
-elif option_chosen == 2: 
-    print('Ativar restaurante')
-else:
-    print('Encerrando o programa...')
+def invalid_option():
+    print('Opção inválida!\n')
+    input('Digite uma tecla para voltar ao menu principal')
+    main()
 
+def choose_option():
+    option_chosen = int(input('Escolha uma opção: '))
+    match option_chosen:
+        case 1:
+            print('Adicionar restaurante')
+        case 2:
+            print('Listar restaurantes')
+        case 3:
+            print('Ativar restaurante')
+        case 4:
+            end_app()
+        case _:
+            invalid_option()
+        
+def main():
+    os.system('cls')
+    display_name()
+    display_options()
+    choose_option()
+
+if __name__ == '__main__':
+    main()
