c06e78fcb0bbfa0b0ba4b61ba760b5b8591c6c77
PxS00 <lucas.rossonidd@gmail.com>
2025-07-25 19:33:51 -0300

    feat: enhance Restaurant model by adding a class-level restaurant list and a method to list all restaurants

--- models/restaurant.py
+++ models/restaurant.py
@@ -1,11 +1,20 @@
 class Restaurant:
+    restaurants = []
+
     def __init__(self, name, category):
         self.name = name
         self.category = category
-        active = False
+        self.active = False
+        Restaurant.restaurants.append(self)
+
+    def __str__(self) -> str:
+        return f'{self.name} | {self.category}'
+    
+    def list_restaurants():
+        for restaurant in Restaurant.restaurants:
+            print(f'{restaurant.name} | {restaurant.category} | {restaurant.active}')
 
 restaurant_pizza_suprema = Restaurant('Pizza Suprema', 'Pizzaria')
 restaurant_grao_e_grelha = Restaurant('Grão & Grelha', 'Hamburguer')
 
-print(vars(restaurant_pizza_suprema))
-print(vars(restaurant_grao_e_grelha))
+Restaurant.list_restaurants()
