4f4a2b9439d7bd8e74c2f475cd14e85a48e80e82
PxS00 <lucas.rossonidd@gmail.com>
2025-07-25 18:54:37 -0300

    feat: add Restaurant model to represent restaurant data with name and category attributes

---
+++ models/restaurant.py
@@ -0,0 +1,10 @@
+class Restaurant:
+    name = ''
+    category = ''
+    active = False
+
+restaurant_pizza_suprema = Restaurant()
+restaurant_pizza_suprema.name = 'Pizza Suprema'
+restaurant_pizza_suprema.category = 'Pizzaria'
+
+print(vars(restaurant_pizza_suprema))
