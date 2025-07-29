2ca33f4f8c2778f68186fc16c6b291bf28396c8d
PxS00 <lucas.rossonidd@gmail.com>
2025-07-29 18:52:54 -0300

    feat: create MenuItem base class and extend it to Dish and Drink models with shared attributes

---
+++ models/menu/dish.py
@@ -0,0 +1,6 @@
+from models.menu.menu_item import MenuItem
+
+class Dish(MenuItem):
+    def __init__(self, name, price, description):
+        super().__init__(name, price)
+        self._description = description
