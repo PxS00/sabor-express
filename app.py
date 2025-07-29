052b3e75bd6afa438b284bb2599a6a7a0ef45242
PxS00 <lucas.rossonidd@gmail.com>
2025-07-29 11:22:47 -0300

    feat: initialize restaurant instances and update main function to list all restaurants

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
+from models.restaurant import Restaurant
 
+restaurant_pizza_suprema = Restaurant('Pizza Suprema', 'Pizzaria')
+restaurant_pizza_suprema.toggle_state()
+restaurant_grao_e_grelha = Restaurant('Grão & Grelha', 'Hamburguer')
+restaurant_na_brasa_cozinha = Restaurant('Na Brasa Cozinha', 'Brasileira')
 
 def main():
-    pass
+    Restaurant.list_restaurants()
 
 if __name__ == '__main__':
     main()
