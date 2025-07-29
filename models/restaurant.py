a5694e54ed87de542d5d9351b96f526e2e57e921
PxS00 <lucas.rossonidd@gmail.com>
2025-07-29 10:37:30 -0300

    refactor: update Restaurant model to use private attributes and improve restaurant listing format

--- models/restaurant.py
+++ models/restaurant.py
@@ -2,18 +2,24 @@ class Restaurant:
     restaurants = []
 
     def __init__(self, name, category):
-        self.name = name
-        self.category = category
-        self.active = False
+        self._name = name.title()
+        self._category = category.upper()
+        self._active = False
         Restaurant.restaurants.append(self)
 
     def __str__(self) -> str:
-        return f'{self.name} | {self.category}'
+        return f'{self._name} | {self._category}'
     
-    def list_restaurants():
-        for restaurant in Restaurant.restaurants:
-            print(f'{restaurant.name} | {restaurant.category} | {restaurant.active}')
+    @classmethod
+    def list_restaurants(cls):
+        print(f'{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'}' )
+        for restaurant in cls.restaurants:
+            print(f'{restaurant._name.ljust(25)} | {restaurant._category.ljust(25)} | {restaurant.active}')
 
+    @property
+    def active(self): 
+        return '⌧' if self._active else '☐'
+    
 restaurant_pizza_suprema = Restaurant('Pizza Suprema', 'Pizzaria')
 restaurant_grao_e_grelha = Restaurant('Grão & Grelha', 'Hamburguer')
 
