35b18e72e9df52b1d54ce3c807b98c64ac9a3174
PxS00 <lucas.rossonidd@gmail.com>
2025-07-30 13:35:53 -0300

    feat: enhance data processing in api.py to organize restaurant items by name and display specific restaurant details

--- api.py
+++ api.py
@@ -9,5 +9,15 @@ if response.status_code == 200:
     data_restaurant = {}
     for item in data_json:
         restaurant_name = item['Company']
+        if restaurant_name not in data_restaurant:
+            data_restaurant[restaurant_name] = []
+        
+        data_restaurant[restaurant_name].append({
+            'item': item['Item'],
+            'price': item['price'],
+            'description': item['description']
+        })
 else:
     print(f'O erro foi {response.status_code}')
+
+print(data_restaurant['McDonald’s'])
