e04c07dde8c70ba1492eee64c4c60798fc89b57c
PxS00 <lucas.rossonidd@gmail.com>
2025-07-30 14:27:08 -0300

    feat: add restaurant menu endpoint to fetch and filter restaurant data

--- main.py
+++ main.py
@@ -1,7 +1,36 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+import requests
 
 app = FastAPI()
 
 @app.get('/api/hello')
 def hello_world():
+    '''
+    Endpoint que exibe uma mensagem incrível do mundo da programação!
+    '''
     return {'Hello':'World'}
+
+@app.get('/api/restaurants/')
+def get_restaurants(restaurant: str = Query(None)):
+    '''
+    Endpoint para ver os cardápios dos restaurantes
+    '''
+    url = 'https://guilhermeonrails.github.io/api-restaurantes/restaurantes.json'
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data_json = response.json()
+        if restaurant is None:
+            return {'Data': data_json}
+
+        data_restaurant = []
+        for item in data_json:
+            if item['Company'] == restaurant:   
+                data_restaurant.append({
+                    'item': item['Item'],
+                    'price': item['price'],
+                    'description': item['description']
+                })
+        return{'Restaurant':restaurant, 'Menu':data_restaurant}
+    else:
+        return {'Erro':f'{response.status_code} - {response.text}'}
